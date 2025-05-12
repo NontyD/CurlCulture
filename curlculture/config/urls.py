5ae224375e6123f2114360a7c0bf43f6f6b3d512
NontyD <nonty@gmail.com>
2025-05-13 00:21:46 +0100

    Add static file serving in development and create CSS styles for layout

--- curlculture/config/urls.py
+++ curlculture/config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),  # For built-in auth views
     path('accounts/', include('accounts.urls')),  # For your custom accounts app
 ]
+
+# Serve static files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
