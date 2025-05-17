a1a913ebd49d0f25f2f41069c4457c13d6a4a8fd
NontyD <nonty@gmail.com>
2025-05-17 23:34:05 +0100

    Add cart functionality: create cart_added view and template; update add_to_cart to handle quantity and redirect to cart_added

--- curlculture/shop/urls.py
+++ curlculture/shop/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('remove-from-cart/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
     path('checkout/', views.checkout, name='checkout'),
+    path('cart-added/', views.cart_added, name='cart_added'),
 ]
