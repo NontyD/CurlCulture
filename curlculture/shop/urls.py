c4e3daa22329c4026dcf82777696e1d55ff222b2
NontyD <nonty@gmail.com>
2025-05-17 22:42:35 +0100

    Add shopping cart functionality: implement cart view, add/remove items, and checkout process; update templates and URLs

--- curlculture/shop/urls.py
+++ curlculture/shop/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
+    path('cart/', views.cart_view, name='cart_view'),
+    path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('remove-from-cart/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('checkout/', views.checkout, name='checkout'),
 ]
