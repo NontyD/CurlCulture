aea72e4f1524d51a5254c154cec298a4b2cbe783
NontyD <nonty@gmail.com>
2025-05-17 21:46:00 +0100

    Add product list view: implement filtering and sorting functionality for products

--- curlculture/shop/views.py
+++ curlculture/shop/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    category = request.GET.get('category')
+    sort = request.GET.get('sort')
+
+    if category:
+        products = products.filter(category__iexact=category)
+    if sort == 'price_asc':
+        products = products.order_by('price')
+    elif sort == 'price_desc':
+        products = products.order_by('-price')
+    elif sort == 'name':
+        products = products.order_by('name')
+
+    categories = Product.objects.values_list('category', flat=True).distinct()
+    return render(request, 'shop/product_list.html', {
+        'products': products,
+        'categories': categories,
+        'selected_category': category,
+        'selected_sort': sort,
+    })
