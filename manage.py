059dca796df228d5e5c7694c76839866220971da
NontyD <nonty@gmail.com>
2025-05-20 19:26:25 +0100

    Update Python version to 3.10 and adjust settings for deployment

--- manage.py
+++ manage.py
@@ -1,11 +1,9 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-
 def main():
-    """Run administrative tasks."""
+    # Add the 'curlculture' directory to the Python path
+    sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), 'curlculture'))
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -16,7 +14,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
