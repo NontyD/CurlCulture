9eed30e604e87ce1e88dfaa8a0afa18d59bbbf84
NontyD <nonty@gmail.com>
2025-05-29 00:25:19 +0100

    Add user profile editing functionality: create UserUpdateForm, edit_profile view, and corresponding template

--- curlculture/accounts/urls.py
+++ curlculture/accounts/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('profile/', views.profile, name='profile'),
+    path('profile/edit/', views.edit_profile, name='edit_profile'),
     path('register/', views.register, name='register'),
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
 ]
