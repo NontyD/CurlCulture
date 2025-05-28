aa67bb0e6fb23426b8454be02e8b8eddcf4cc82a
NontyD <nonty@gmail.com>
2025-05-29 00:47:22 +0100

    Add sitemap and robots.txt configuration: create StaticViewSitemap class, update URLs for sitemap and robots.txt

---
+++ curlculture/home/sitemaps.py
@@ -0,0 +1,12 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+
+class StaticViewSitemap(Sitemap):
+    priority = 1.0
+    changefreq = 'daily'
+
+    def items(self):
+        return ['home', 'shop', 'contact', 'reviews']  # Add your named URLs
+
+    def location(self, item):
+        return reverse(item)
